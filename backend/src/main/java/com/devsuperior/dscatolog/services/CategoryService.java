3ea611564393110450fe1ec3e172c1a8168545f2
Moisez Dantas <moisez.dantas@visto.global>
2020-09-29 18:33:10 -0300

    add database seeding

--- backend/src/main/java/com/devsuperior/dscatolog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatolog/services/CategoryService.java
@@ -2,8 +2,11 @@ package com.devsuperior.dscatolog.services;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatolog.entities.Category;
 import com.devsuperior.dscatolog.repositories.CategoryRepository;
@@ -14,6 +17,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll(){
 		return categoryRepository.findAll();
 	}
