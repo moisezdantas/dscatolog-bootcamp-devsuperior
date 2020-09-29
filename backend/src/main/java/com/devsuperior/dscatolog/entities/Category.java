bc88ad6a357d373e279c12512f953ba96f5bf332
Moisez Dantas <moisez.dantas@visto.global>
2020-09-29 18:20:49 -0300

    add h2, service, repository

--- backend/src/main/java/com/devsuperior/dscatolog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatolog/entities/Category.java
@@ -2,10 +2,23 @@ package com.devsuperior.dscatolog.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name =  "tb_category")
 public class Category implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column
 	private String name;
 	
 	public Category() {}
--- backend/src/main/java/com/devsuperior/dscatolog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatolog/resources/CategoryResource.java
@@ -1,27 +1,26 @@
 package com.devsuperior.dscatolog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatolog.entities.Category;
+import com.devsuperior.dscatolog.services.CategoryService;
 
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
+	
+	@Autowired
+	private CategoryService categoryService;
 
 	@GetMapping
 	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = new ArrayList<>();
-		
-		list.add(new Category(1l, "Books"));
-		list.add(new Category(2l, "Electonics"));
-		
-		return ResponseEntity.ok().body(list);
+		return ResponseEntity.ok().body(categoryService.findAll());
 	}
 }
