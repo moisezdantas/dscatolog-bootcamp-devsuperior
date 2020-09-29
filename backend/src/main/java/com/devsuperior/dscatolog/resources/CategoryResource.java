0eb4685df5ec1081685f13431b6de942b2ebcc59
Moisez Dantas <moisez.dantas@visto.global>
2020-09-29 17:52:09 -0300

    add class category resource

---
+++ backend/src/main/java/com/devsuperior/dscatolog/resources/CategoryResource.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dscatolog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscatolog.entities.Category;
+
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll(){
+		List<Category> list = new ArrayList<>();
+		
+		list.add(new Category(1l, "Books"));
+		list.add(new Category(2l, "Electonics"));
+		
+		return ResponseEntity.ok().body(list);
+	}
+}
