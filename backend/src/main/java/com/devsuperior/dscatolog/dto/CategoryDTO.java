1adea772b4447acd7f672629b57af5e027d3b335
Moisez Dantas <moisez.dantas@visto.global>
2020-09-30 18:43:30 -0300

    Post new Category

--- backend/src/main/java/com/devsuperior/dscatolog/dto/CategoryDTO.java
+++ backend/src/main/java/com/devsuperior/dscatolog/dto/CategoryDTO.java
@@ -20,7 +20,7 @@ public class CategoryDTO implements Serializable{
 	
 	public CategoryDTO(Category entity) {
 		this.id = entity.getId();
-		this.name =  entity.getName();
+		this.name = entity.getName();
 	}
 	
 	public Long getId() {
--- backend/src/main/java/com/devsuperior/dscatolog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatolog/services/CategoryService.java
@@ -33,4 +33,12 @@ public class CategoryService {
 	
 	}
 
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = categoryRepository.save(entity);
+		return new CategoryDTO(entity);
+	}
+	
 }
