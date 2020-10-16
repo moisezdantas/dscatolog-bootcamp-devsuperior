7c408415ef2a01be8a4e5219bfec96e39e132bce
Moisez Dantas <moisez.dantas@visto.global>
2020-10-16 19:54:05 -0300

    add base admin form structure

---
+++ front-web/src/pages/Admin/components/BaseForm/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./styles.scss";
+
+type Props = {
+  title: string;
+  children: React.ReactNode;
+};
+
+const BaseForm = ({ title, children }: Props) => {
+  const history = useHistory();
+
+  const handleCancel = () => {
+    history.push("../");
+  };
+
+  return (
+    <div className="admin-base-form card-base">
+      <h1 className="base-form-title">{title}</h1>
+      {children}
+      <div className="base-form-actions">
+        <button
+          className="btn btn-outline-danger btn-radius-10 mr-3"
+          onClick={handleCancel}
+        >
+          CANCELAR
+        </button>
+        <button className="btn btn-primary btn-radius-10 ">CADASTRAR</button>
+      </div>
+    </div>
+  );
+};
+export default BaseForm;
--- front-web/src/pages/Admin/components/Products/index.tsx
+++ front-web/src/pages/Admin/components/Products/index.tsx
@@ -1,24 +1,17 @@
 import React from "react";
-import { Route, Switch,Link } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
+import Form from "./Form";
+import List from "./List";
 
 const Products = () => {
   return (
     <div>
-        <Link to="/admin/products" className="mr-5">
-            Listar Produtos
-        </Link>
-        <Link to="/admin/products/create" className="mr-5">
-            Criar Produtos
-        </Link>
-        <Link to="/admin/products/1" className="mr-5">
-            Editar Produtos
-        </Link>
       <Switch>
         <Route path="/admin/products" exact>
-          <h1>Exibir a listagem de produtos</h1>
+          <List/>
         </Route>
         <Route path="/admin/products/create">
-          <h1>Criar novo produto</h1>
+          <Form />
         </Route>
         <Route path="/admin/products/:productId">
           <h1>Editar um produto</h1>
