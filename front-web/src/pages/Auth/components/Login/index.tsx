6d8dadc1d5dceae4b462f58fe426715039a00b24
Moisez Dantas <moisez.dantas@visto.global>
2020-11-02 18:18:25 -0300

    add allowed roles on private route

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import AuthCard from "../Card";
 import ButtonIcon from "core/components/ButtonIcon";
@@ -12,17 +12,25 @@ type FormData = {
   password: string;
 };
 
+type LocationState = {
+  from: string;
+}
+
 const Login = () => {
   const { register, handleSubmit , errors} = useForm<FormData>();
   const [hasError, setHasError] = useState(false);
   const history = useHistory();
+
+  let location = useLocation<LocationState>();
+
+  const { from } = location.state || { from: { pathname: "/admin" } };
 
   const onSubmit = (data: FormData) => {
     makeLogin(data)
       .then((response) => {
         setHasError(false);
         saveSessionData(response.data);
-        history.push("/admin");
+        history.replace(from);
       })
       .catch(() => {
         setHasError(true);
