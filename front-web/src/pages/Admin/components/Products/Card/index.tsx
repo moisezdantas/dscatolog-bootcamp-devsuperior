11289317f064d881cb46a518260a77055e164278
Moisez Dantas <moisez.dantas@visto.global>
2020-11-21 08:38:24 -0300

    Integrate product listing with backend

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,24 +1,30 @@
-import ProductPrice from 'core/components/ProductPrice';
 import React from 'react';
+import ProductPrice from 'core/components/ProductPrice';
+import { Product } from 'core/types/Product';
 import './styles.scss';
 
-const Card = () => {
+type Props = {
+    product:Product;
+}
+
+const Card = ({product}: Props) => {
     return (
         <div className="card-base product-card-admin">
             <div className="row">
                 <div className="col-2 text-center border-right py-3">
-                    <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg" 
-                    alt="imagem" 
-                    className="product-card-image-admin"/>
+                    <img 
+                        src={product.imgUrl} 
+                        alt={product.name}
+                        className="product-card-image-admin"
+                    />
                 </div>
                 <div className="col-7 py-3">
-                    <h3 className="product-card-name-admin">Computador </h3>
-                    <ProductPrice price={40.5}/>
+                    <h3 className="product-card-name-admin">{product.name} </h3>
+                    <ProductPrice price={product.price}/>
                     <div>
-                        <span className="badge badge-pill badge-secondary mr-2">Categoria</span>
-                        <span className="badge badge-pill badge-secondary mr-2">Categoria 1</span>
-                        <span className="badge badge-pill badge-secondary mr-2">Categoria 2</span>
-                        <span className="badge badge-pill badge-secondary mr-2">Categoria 3 </span>
+                       {product.categories.map(category => 
+                            <span className="badge badge-pill badge-secondary mr-2">{category.name}</span>
+                       )}
                     </div>
                 
                 </div>
