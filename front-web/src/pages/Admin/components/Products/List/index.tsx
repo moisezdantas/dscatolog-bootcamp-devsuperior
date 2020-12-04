f00658a2f983ed6587f52caa2d3a080e975fbfbd
Moisez Dantas <moisez.dantas@visto.global>
2020-12-04 18:59:47 -0300

    add delete product

--- front-web/src/pages/Admin/components/Products/List/index.tsx
+++ front-web/src/pages/Admin/components/Products/List/index.tsx
@@ -1,8 +1,9 @@
 import Pagination from "core/components/Pagination";
 import { ProductsResponse } from "core/types/Product";
-import { makeRequest } from "core/utils/request";
-import React, { useEffect, useState } from "react";
+import { makePrivateRequest, makeRequest } from "core/utils/request";
+import React, { useCallback, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import { toast } from "react-toastify";
 import Card from "../Card";
 import CardLoader from "../Loaders/ProductCardLoader";
 
@@ -12,7 +13,7 @@ const List = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [activePage, setActivePage] = useState(0);
 
-  useEffect(() => {
+  const getProducts = useCallback(() => {
     const params = {
       page: activePage,
       linesPerPage: 4,
@@ -28,8 +29,26 @@ const List = () => {
       });
   }, [activePage]);
 
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
   const handleCreate = () => {
     history.push("/admin/products/create");
+  };
+
+  const onRemove = (productId: number) => {
+    const confirm = window.confirm("Deseja realmente excluir este produto?");
+    if (confirm) {
+      makePrivateRequest({ url: `/products/${productId}`, method: "DELETE" })
+        .then(() => {
+          toast.info("Produto removido com sucesso!");
+          getProducts();
+        })
+        .catch(() => {
+          toast.error("Erro ao remover produto");
+        });
+    }
   };
 
   return (
@@ -43,7 +62,9 @@ const List = () => {
           <CardLoader />
         ) : (
           productsResponse?.content.map((product) => {
-            return <Card product={product} key={product.id} />;
+            return (
+              <Card product={product} key={product.id} onRemove={onRemove} />
+            );
           })
         )}
       </div>
