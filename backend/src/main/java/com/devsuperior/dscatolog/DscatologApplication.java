bbcf02467034423d4e7f88a6d58845a5b00d92ab
Moisez Dantas <moisez.dantas@visto.global>
2020-12-29 20:41:40 -0300

    First upload test

--- backend/src/main/java/com/devsuperior/dscatolog/DscatologApplication.java
+++ backend/src/main/java/com/devsuperior/dscatolog/DscatologApplication.java
@@ -1,13 +1,27 @@
 package com.devsuperior.dscatolog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.devsuperior.dscatolog.services.S3Service;
+
 @SpringBootApplication
-public class DscatologApplication {
+public class DscatologApplication implements CommandLineRunner{
+	
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DscatologApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+			
+		s3Service.uploadFile("/home/moisez_dantas/Imagens/nova.png");
+		
 	}
 
 }
